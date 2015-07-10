9b8230c2c37ea50519a0e4e6279d50a2b4f6e17d
kldon111 <ilavaboyi@gmail.com>
2015-07-11 01:22:56 +0400

    kitchen
    kitchen

---
+++ plugin.video.kitchen/default.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+try:import sys, syspath
+except:pass
+from plugintools import *
+
+
+def infolist():
+  list1=[]
+  list1.append(('Betty Kitchen','UCu41XMEGBUSr7k_d5oDZ84w'))
+  list1.append(('24kitchen','UCkj20NOg2aCDJ8EtF_ep5jg'))
+  list1.append(('elgourmet','UC1Lhubbf3BjYODUrugx-oeA'))
+  list1.append(('Roya Kitchen&روية طبخ','UCR5RnJywFDicUFcPlsQ0jFw'))
+  list1.append(('ChefAhmad AllCooking&الشيف احمد طبخ','UC7zsrALcfQ0k8BWH2tGE5JQ'))
+  list1.append(('Manal Alalem&منال العالم طبخ','UCMM7pqZRzJIWk-YZiH5eKIg'))
+  list1.append(('مطبخ دلال الحناوي','UCV9YIkOaEi5AXSTLkV7fWYQ'))
+  list1.append(('Fatafeat&فتافيت طبخ','UCU8vqcbu1R04Wt9_sz8Fh3g'))
+  list1.append(('مطبخ ديزى DaisyKitchen','UCm6zvhnzpdMvjSVrlgHlVZA'))
+  list1.append(('الشيف أبو علوش','UC_lFrM_JAXzF-WfaOmG02PQ'))
+  list1.append(('Assia Kitchen&طبخ عربي','UCOu7FnEDAtuppol-TCMThiA'))
+  list1.append(('CBC Sofra&سفرة طبخ','UCCdCsHQRURiTXwDwwW6fx0w'))
+  list1.append(('قناة ست البيت SEBTV','UCDE8a5b85mMucnUYVtKT1zQ'))
+  list1.append(('مطبخ الاكلات العراقية','UCAb1TYmYbD7pfWpW1NK8-2g'))
+  list1.append(('amal elmziryahi&امل طبخ','UCm6ObKyiIf7fX_ELxXyCStA'))
+  list1.append(('الشيف عامر','UCYGHf96lNSzDrbNfMccfpJg'))
+  return list1
+
+################################################################333
+list1=infolist()
+process_mode(list1 )
+
+
