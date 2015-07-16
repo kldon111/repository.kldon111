88895a52f2e432c760817af84fea14f314b337ca
kldon111 <ilavaboyi@gmail.com>
2015-07-16 04:32:34 +0400

    samira tv
    samira tv

---
+++ plugin.video.samiratv/default.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+try:import sys,syspath
+except:pass
+from plugintools import *
+
+
+def infolist():
+  list1=[]
+  list1.append(('Samira TV','UCofQX49-chJUTKFREfdQHew'))
+  list1.append(('Samira TV2','UCE-GgDV9UefoStG8uHrMCAw'))
+  list1.append(('Samira TV3','UCpj3QAnoBH4L-P0aSVuFRrA'))
+  list1.append(('samira tv 1','UCjlAtzVYTHMI6YCVLfgCMVw'))
+  list1.append(('chaine samira','UCFZU_OfGc-iuAh_eCBYJX_Q'))
+  list1.append(('بنـّة Bena TV','UCJ4ITxwi1Mi5gg8LTVhoNpA'))
+  list1.append(('طبخ ليلى tabkh leila','UC_BUnj-MP9154Vsahdq8dJQ'))
+  list1.append(('ريحانة كمال','UCSZCXMvv1p5kurtJmaxKikA'))
+  list1.append(('الشيف نادية','UCb34vKlc-884MuwTrg_q7Zw'))
+  list1.append(('المطبخ المغربي مع ربيعة','UCFldnUF7Y4JbUf7C-wkwSEA'))
+  list1.append(('حلويات خديجة halawiyat khadija','UCXAv5LtMlXFCjnc03uBqBwQ'))
+  list1.append(('شهيوات نينا Recettes Faciles','UCxRCTCsLEfA8uWYm84C2hXQ'))
+  list1.append(('Manal Alalem','UCMM7pqZRzJIWk-YZiH5eKIg'))
+  return list1
+ 
+
+################################################################333
+list1=infolist()
+process_mode(list1 )
+
+
